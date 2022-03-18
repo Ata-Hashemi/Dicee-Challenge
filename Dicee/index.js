f04d57735ec1c21fd66fc80bb4fefb85b980dc87
Ata <82298093+Ata-Hashemi@users.noreply.github.com>
2022-03-18 17:36:02 -0400

    Update index.js

--- Dicee/index.js
+++ Dicee/index.js
@@ -14,7 +14,7 @@ if (randomNumber1<randomNumber2) {
     document.querySelector("h1").innerHTML="Player 2 wins!";
 }
 
-else if(randomNumber1>randomNumber2) {
+else if (randomNumber1>randomNumber2) {
     document.querySelector("h1").innerHTML="Player 1 wins!";
 }
 
